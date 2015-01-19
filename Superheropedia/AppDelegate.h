be24137eb8e5f6beb368a222d551bf3f71cabf73
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-01-19 10:32:30 -0600

    Initial Commit

---
+++ Superheropedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Superheropedia
+//
+//  Created by Kyle Magnesen on 1/19/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
